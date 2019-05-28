1bd955c15ff0879812aaa9fa30520c92476cb325
andraderaul <theandraderaul@gmail.com>
2019-05-28 17:23:57 -0300

    processamento de imagem

---
+++ pimg.py
@@ -0,0 +1,22 @@
+'''
+    ndarray - imagem
+    string - nome de arquivo
+    os.listdir(“.”) - lista os arquivos do diretório “.”, ou seja, do diretório atual.
+    os.chdir(“..”) - Acessa o diretório “..”, ou seja, o pai do diretório atual.
+    os.getcwd() - Mostra o nome do diretório atual.
+
+    referencias:
+    # http://www.galirows.com.br/meublog/opencv-python/opencv2-python27/capitulo1-basico/mostrar-imagem-opencv-python/
+    # https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.ndarray.html
+    # https://matplotlib.org/users/image_tutorial.html
+    # http://www.scipy-lectures.org/advanced/image_processing/
+    # https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/imaging.html
+    # https://matplotlib.org/gallery/images_contours_and_fields/interpolation_methods.html
+    # http://www.degeneratestate.org/posts/2016/Oct/23/image-processing-with-numpy/
+
+'''
+'''
+    Aluno: Raul Andrade
+    Matricula: 201500307353
+    E-mail: raul.andrade@dcomp.ufs.br
+'''
